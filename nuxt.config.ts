5198310fd43a62af529ff2748e03891eeb1aa022
Becasuse-hung <4a33a006@stust.edu.tw>
2024-02-08 01:28:57 +0800

    feat: SEO + meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    // "components": {  // 指定特定的組件 auto import (在 global 資料夾下的組件才有 auto import 的功能)
-    //   "dirs": [
-    //     {
-    //         "path": "~/components/global",
-    //         "global": true
-    //     }
-    //   ]
-    // },
-    "imports": { // 設定新增 特定資料夾有 auto import 的功能
-      "dirs": ['stores']
-    }, 
-    "modules": ['@pinia/nuxt'],
-    devtools: { enabled: true }
+  app: {
+    head: {
+      viewport: "width=500, initial-scale=1",
+      title: "Nuxt3 高效入門全攻略",
+      meta: [
+        {
+          name: "description",
+          content: "這是 Mike 的 Nuxt3 高效入門全攻略課程",
+        },
+        { property: "og:title", content: "Nuxt3 高效入門全攻略" },
+        { property: "og:url", content: "http://localhost:3000/" },
+        {
+          property: "og:description",
+          content: "這是 Mike 的 Nuxt3 高效入門全攻略課程",
+        },
+      ],
+      // link: [
+      //   {
+      //     rel: "stylesheet",
+      //     href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
+      //   },
+      // ],
+      // script: [
+      //   {
+      //     src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js",
+      //     async: true,
+      //   },
+      // ],
+    },
+  },
+  // "components": {  // 指定特定的組件 auto import (在 global 資料夾下的組件才有 auto import 的功能)
+  //   "dirs": [
+  //     {
+  //         "path": "~/components/global",
+  //         "global": true
+  //     }
+  //   ]
+  // },
+  imports: {
+    // 設定新增 特定資料夾有 auto import 的功能
+    dirs: ["stores"],
+  },
+  modules: ["@pinia/nuxt"],
+  devtools: { enabled: true },
 })
-
--- pages/about.vue
+++ pages/about.vue
@@ -1,4 +1,52 @@
-<script setup></script>
+<script setup>
+// useServerSeoMeta - 初始加載一開始餵給 google seo 的資訊會改變, 但 client 端 不會有改變
+const res = await useFetch("https://vue-lessons-api.vercel.app/seo/about");
+
+useServerSeoMeta({
+  title: () => `${res.data.value.title} - Nuxt3`,
+  ogTitle: () => `${res.data.value.title} - Nuxt3`,
+  description: () => `${res.data.value.description} - Nuxt3`,
+  ogDescription: () => `${res.data.value.description} - Nuxt3`,
+});
+
+// 這是單頁面引入 要全域引入要寫在 nuxt.config.ts 裡
+useHead({
+  link: [
+    {
+      rel: "stylesheet",
+      href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
+    },
+  ],
+  script: [
+    {
+      src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js",
+      async: true,
+    },
+  ],
+});
+
+// useHead
+// useHead({
+//  title: "關於我們 About - Nuxt3 高效入門全攻略",
+//  meta: [
+//    { property: "og:title", content: "關於我們 About - Nuxt3 高效入門全攻略" },
+//    { property: "og:url", content: "http://localhost:3000/about" },
+//    { property: "og:image", content: "http://localhost:3000/share.jpg" },
+//    { name: "description", content: "關於我們 About - 最棒的Nuxt3的線上課程" },
+//    { property: "og:description", content: "關於我們 About - 最棒的Nuxt3的線上課程" },
+//  ],
+// });
+
+// useSeoMeta
+// useSeoMeta({
+//  title: "About - Nuxt3 高效入門全攻略",
+//  description: "關於我們 - 最棒的Nuxt3的線上課程",
+//  ogDescription: "關於我們 - 最棒的Nuxt3的線上課程",
+//  ogTitle: "About - Nuxt3 高效入門全攻略",
+// });
+
+
+</script>
 
 <template>
   <div>
